6608b4691cacc131533e12dbf56f452c91b01608
liuhao <867790763@qq.com>
2025-06-13 17:52:55 +0800

    从cookie读取token

--- my-frontEnd/src/router/permission.ts
+++ my-frontEnd/src/router/permission.ts
@@ -1,29 +1,26 @@
 import type { Router } from "vue-router";
 import { useUserStore } from "@/stores/modules/user";
 import { PageEnum } from '@/enums/pageEnum';
-
+import { getCookie } from '@/utils/auth'
 const LOGIN_PATH = PageEnum.BASE_LOGIN;
 // 设置白名单
 const whitePathList: PageEnum[]= [LOGIN_PATH]
 
 export const createPermission = (router: Router) => {
   const userStore = useUserStore()
-  const hasToken = userStore.getToken as string
-  console.log(!hasToken);
-  console.log(hasToken);
+  const hasToken = getCookie()
   router.beforeEach((to, from, next) => {
+    console.log(hasToken?.length);
+  console.log(hasToken);
     if (whitePathList.includes(to.path as PageEnum)) {
       next();
       return;
     }
-    if(!hasToken) {
-      // console.log(hasToken);
+    if(hasToken?.length === 0) {
+      console.log(hasToken);
       router.push({ path: LOGIN_PATH })
-    }else {
-      console.log('123456');
-      
-      next()
-      return;
     }
+    next();
+    return;
   })
 }
