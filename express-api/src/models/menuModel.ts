7b22697624df9fef36e6ce522f5a09f4c4fb2c3a
liuhao <867790763@qq.com>
2026-03-02 16:44:59 +0800

    1.处理登录密码加解密 2.修改菜单鲁姆路径

--- express-api/src/models/menuModel.ts
+++ express-api/src/models/menuModel.ts
@@ -25,8 +25,17 @@ export const menuList: Menu[] = [
     icon: 'UserOutlined',
     component: "LAYOUT",
     children: [
-      { key: 'list', label: '用户管理', component: '/sys/user/index', path: '/sys/user', url: '/sys/user/index' },
+      { key: 'list', label: '用户管理', component: '/sys/user/list', path: '/sys/user', url: '/sys/user/list' },
       { key: 'roles', label: '角色管理', component: '/sys/role/index', path: '/sys/role', url: '/sys/role/index' },
+    ],
+  },
+  {
+    key: 'bim',
+    label: '3D管理',
+    icon: 'UserOutlined',
+    component: "LAYOUT",
+    children: [
+      { key: 'examples', label: '3D示例', component: '/examples/demo', path: '/examples', url: '/examples/demo' },
     ],
   },
 ];
--- express-api/src/utils/cipher.ts
+++ express-api/src/utils/cipher.ts
@@ -1,6 +1,19 @@
 const UTF8 = require('crypto-js/enc-utf8');
 const Base64 = require('crypto-js/enc-base64');
+const Md5 = require('crypto-js/md5');
+export interface EncryptionParams {
+  key: string;
+  iv: string;
+}
+
+export function encryptByBase64(cipherText: string) {
+  return UTF8.parse(cipherText).toString(Base64);
+}
 
 export function decryptByBase64(cipherText: string) {
   return Base64.parse(cipherText).toString(UTF8);
+}
+
+export function encryptByMd5(password: string) {
+  return Md5(password).toString();
 }
